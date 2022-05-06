fc92f2bd25367d7bbad3773568d2e4693fae3951
alm <alm.dev2@gmail.com>
2022-05-07 01:24:34 +0200

    Update mappings.lua

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,40 +1,42 @@
-local map = vim.api.nvim_set_keymap
-local map_buf = vim.api.nvim_buf_set_keymap
-local opts = { noremap=true, silent=true }
+set completeopt=menu,menuone,noselect
+syntax enable
+filetype plugin on
+
+set number relativenumber
+" set acd " auto change directory
+set cmdheight=1
+set cursorline
+set nohlsearch
+set nowrap
+set signcolumn=yes
+set splitbelow
+set splitright
+
+if has('termguicolors')
+  set termguicolors
+endif
+
+" mouse cursor
+set mouse=n
+
+" tab behaviour
+set shiftwidth=2  " operation >> indents 2 columns; << unindents 2 columns
+set tabstop=2     " a hard TAB displays as 2 columns
+set softtabstop=2 " insert/delete 2 spaces when hitting a TAB/BACKSPACE
+set expandtab
+
+" gruvbox-material config
+" available values: 'hard', 'medium'(default), 'soft'
+let g:gruvbox_material_background="medium"
+let g:gruvbox_material_sign_column_background="none"
+
+colorscheme  gruvbox-material
+
+let g:rust_recommended_style = 0
+highlight! link DiagnosticInfo DiagnosticSignInfo 
+highlight! link DiagnosticError DiagnosticSignError
+highlight! link DiagnosticWarn DiagnosticSignWarn
+highlight! link DiagnosticHint DiagnosticSignHint
+lua require('init')
 
 
--- Lsp Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-map('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-map('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-
-
--- Telescope Mappings
-map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts)
-map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)
-map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', opts)
-map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', opts)
-
-
--- Barbar Mappings
--- Move to previous/next
-map('n', '<A-,>', ':BufferPrevious<CR>', opts)
-map('n', '<A-.>', ':BufferNext<CR>', opts)
--- Re-order to previous/next
-map('n', '<A-<>', ':BufferMovePrevious<CR>', opts)
-map('n', '<A->>', ' :BufferMoveNext<CR>', opts)
--- Goto buffer in position...
-map('n', '<A-1>', ':BufferGoto 1<CR>', opts)
-map('n', '<A-2>', ':BufferGoto 2<CR>', opts)
-map('n', '<A-3>', ':BufferGoto 3<CR>', opts)
-map('n', '<A-4>', ':BufferGoto 4<CR>', opts)
-map('n', '<A-5>', ':BufferGoto 5<CR>', opts)
-map('n', '<A-6>', ':BufferGoto 6<CR>', opts)
-map('n', '<A-7>', ':BufferGoto 7<CR>', opts)
-map('n', '<A-8>', ':BufferGoto 8<CR>', opts)
-map('n', '<A-9>', ':BufferGoto 9<CR>', opts)
-map('n', '<A-0>', ':BufferLast<CR>', opts)
--- Close buffer
-map('n', '<A-c>', ':BufferClose<CR>', opts)
